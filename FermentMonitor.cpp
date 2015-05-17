116218f2cd5e192fffa30ad5b11c0925ffae779b
Peter Buelow <goballstate@gmail.com>
2015-05-17 10:34:42 -0500

    Oops, bad label

--- FermentMonitor.cpp
+++ FermentMonitor.cpp
@@ -83,7 +83,7 @@ void FermentMonitor::getDHTValues()
 	QString s2;
 	if (dhtMon.isValid()) {
 		s1 = QString().setNum(dhtMon.getTemperature(), 'f', 1);
-		lbBoxTemp->setText(QString("<font style='font-size:20pt;'>Humidity</font><br><font style='font-size:48pt; color:black'>%1</font> <font style='font-size:20pt'>%2F</font>").arg(s1));
+		lbBoxTemp->setText(QString("<font style='font-size:20pt;'>Temperature</font><br><font style='font-size:48pt; color:black'>%1</font> <font style='font-size:20pt'>%2F</font>").arg(s1));
 		s2 = QString().setNum(dhtMon.getHumidity(), 'f', 1);
 		lbExternalTemp->setText(QString("<font style='font-size:20pt;'>Humidity</font><br><font style='font-size:48pt; color:black'>%1</font> <font style='font-size:20pt'>%2F</font>").arg(s2));
 	}
